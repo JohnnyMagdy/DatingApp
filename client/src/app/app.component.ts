8953f259fc13b937f0135a7fe77b8c8e5ae8cfd8
Johnny <j.magdy18@yahoo.com>
2022-02-03 13:18:19 +0200

    Adding navbar, registration and home page.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,26 +12,17 @@ export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error=>{
-      console.log(error);
-    })
-
-    // this.http.get('https://localhost:5001/API/Users').subscribe({
-    //   next: response => {
-    //     this.users = response;
-    //   },
-    //   error: error => {
-    //     console.error(error);
-    //   }
-    // });
+  setCurrentUser(){
+    let myuser = localStorage.getItem('user');
+    if (myuser){
+      const user: User = JSON.parse(myuser);
+      this.accountService.setCurrentUser(user);
+    }
   }
 }
